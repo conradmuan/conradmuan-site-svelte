568f6a16051a97b5190766a95d5a6804330a83b5
Conrad Muan <hi@conrad.chat>
2022-12-15 21:58:21 -0800

    trying to make this work with gh pages

--- build/_app/immutable/chunks/6-c9a6466a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/admin/fields/_id_/_page.svelte-7a3d579a.js";const e=!0;export{t as component,e as server};
--- build/_app/immutable/chunks/7-84c216f1.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/admin/fields/create/_page.svelte-ac69b4ec.js";export{t as component};
