4ca143ecc06b319e0f7294e11ffb5858a79be0ec
Conrad Muan <hi@conrad.chat>
2022-12-15 21:58:21 -0800

    trying to make this work with gh pages

--- build/_app/immutable/chunks/10-ef5408fa.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/admin/posts/create/_page.svelte-b1074393.js";export{t as component};
---
+++ docs/_app/immutable/chunks/10-4dd8c0ed.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/admin/posts/create/_page.svelte-6f6b2574.js";export{t as component};
