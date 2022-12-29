cc9608a338a05973963990ded7263fd518659fde
Conrad Muan <hi@conrad.chat>
2022-12-29 12:52:19 -0800

    built blog post

---
+++ docs/_app/immutable/chunks/6-fcbc1fb9.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/(admin)/admin/books/create/_page.svelte-3ef45446.js";export{t as component};
---
+++ docs/_app/immutable/chunks/date-b67229a3.js
@@ -0,0 +1,1 @@
+const s=e=>{const n=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],t=new Date(e),o=t.getFullYear(),a=n[t.getMonth()],c=t.getDate();return`${a} ${c}, ${o}`};export{s as n};
